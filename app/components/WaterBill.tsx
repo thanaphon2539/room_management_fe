ec7ed8b987e9cdf1ece2768d8d123b81f865f3b1
Sirinan <sirinanp64@gmail.com>
2025-01-30 23:44:29 +0700

    add page

--- app/components/WaterBill.tsx
+++ app/components/WaterBill.tsx
@@ -1,71 +1,69 @@
 import { useState } from "react";
 import CreateRoom from "./CreateRoom";
 
-interface Room {
+interface Bill {
   id: number;
   name: string;
   status: string;
   userType: string;
+  oldBill: number;
+  bill: number;
+  totalBill: number;
 }
 
-export default function RoomList() {
-  const [rooms, setRooms] = useState<Room[]>([
+export default function BillList() {
+  const [bills, setBills] = useState<Bill[]>([
     {
       id: 1,
       name: "John Doe",
-      status: "john@example.com",
+      status: "",
       userType: "legalEntity",
-    },
-    {
-      id: 2,
-      name: "Jane Smith",
-      status: "jane@example.com",
-      userType: "person",
-    },
-    {
-      id: 3,
-      name: "Jane Smith",
-      status: "jane@example.com",
-      userType: "",
+      oldBill: 0,
+      bill: 0,
+      totalBill: 0
     },
   ]);
   const [showCreateForm, setShowCreateForm] = useState(false);
-  const [editingRoom, setEditingRoom] = useState<Room | null>(null);
-  const [deleteRoomId, setDeleteRoomId] = useState<number | null>(null);
+  const [editingBill, setEditingBill] = useState<Bill | null>(null);
+  const [deleteBillId, setDeleteBillId] = useState<number | null>(null);
 
   // เพิ่ม room
-  const addRoom = (newRoom: Room) => {
-    setRooms([...rooms, newRoom]);
+  const addBill = (newBill: Bill) => {
+    setBills([...bills, newBill]);
     setShowCreateForm(false);
   };
 
   // แสดง popup ยืนยันการลบ
   const confirmDeleteRoom = (id: number) => {
-    setDeleteRoomId(id);
+    setDeleteBillId(id);
   };
 
   // ลบ
   const handleDelete = () => {
-    if (deleteRoomId !== null) {
-      setRooms(rooms.filter((user) => user.id !== deleteRoomId));
-      setDeleteRoomId(null);
+    if (deleteBillId !== null) {
+      setBills(bills.filter((bill) => bill.id !== deleteBillId));
+      setDeleteBillId(null);
     }
   };
 
   // อัปเดตข้อมูลผู้ใช้
-  const updateRoom = (updatedRoom: Room) => {
-    setRooms(
-      rooms.map((user) => (user.id === updatedRoom.id ? updatedRoom : user))
+  const updateRoom = (updatedRoom: Bill) => {
+    setBills(
+      bills.map((bill) => (bill.id === updatedRoom.id ? updatedRoom : bill))
     );
     setEditingRoom(null);
   };
+
+  function updateBill(user: Bill): void {
+    throw new Error("Function not implemented.");
+  }
 
   return (
     <div className="container">
       <div className="card">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-2xl font-bold">รายการห้องเช่า</h2>
-          {!editingRoom && !showCreateForm && (
+          {!editingBill && !showCreateForm && (
             <button
               onClick={() => setShowCreateForm(true)}
               className="btn-base"
@@ -77,11 +75,11 @@ export default function RoomList() {
 
         {showCreateForm ? (
           <CreateRoom addRoom={addRoom} />
-        ) : editingRoom ? (
-          <EditRoom
-            room={editingRoom}
-            updateRoom={updateRoom}
-            setEditingRoom={setEditingRoom}
+        ) : editingBill ? (
+          <EditBill
+            bill={editingBill}
+            updateBill={updateBill}
+            setEditingBill={setEditingBill}
           />
         ) : (
           <div className="base-table">
@@ -91,33 +89,20 @@ export default function RoomList() {
                   <th>ชื่อห้อง</th>
                   <th>สถานะ</th>
                   <th>ประเภท</th>
-                  <th>จัดการ</th>
+                  <th>ค่าน้ำเดือนที่แล้ว</th>
+                  <th>ค่าน้ำเดือนปัจจุบัน</th>
+                  <th>หน่วย</th>
                 </tr>
               </thead>
               <tbody>
-                {rooms.map((row) => (
+                {bills.map((row) => (
                   <tr key={row.id}>
                     <td>{row.name}</td>
                     <td>{row.status}</td>
                     <td>{row.userType}</td>
-                    <td>
-                      <div className="flex justify-center">
-                        <button
-                          onClick={() => setEditingRoom(row)}
-                          className="bg-yellow-500 text-white px-3 py-1 rounded mr-2 hover:bg-yellow-600"
-                        >
-                          <i className="bi bi-pencil-fill" />
-                          แก้ไข
-                        </button>
-                        <button
-                          onClick={() => confirmDeleteRoom(row.id)}
-                          className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600"
-                        >
-                          <i className="bi bi-trash" />
-                          ลบ
-                        </button>
-                      </div>
-                    </td>
+                    <td>{row.oldBill}</td>
+                    <td>{row.bill}</td>
+                    <td>{row.totalBill}</td>
                   </tr>
                 ))}
               </tbody>
@@ -127,14 +112,14 @@ export default function RoomList() {
       </div>
 
       {/* Popup Modal สำหรับยืนยันการลบ */}
-      {deleteRoomId !== null && (
+      {deleteBillId !== null && (
         <div className="fixed inset-0 flex items-center justify-center bg-opacity-50">
           <div className="bg-white p-6 rounded-lg shadow-lg w-96">
             <h2 className="text-xl font-bold mb-4">Confirm Deletion</h2>
             <p>Are you sure you want to delete this user?</p>
             <div className="flex justify-end mt-4">
               <button
-                onClick={() => setDeleteRoomId(null)}
+                onClick={() => setDeleteBillId(null)}
                 className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 mr-2"
               >
                 Cancel
@@ -154,18 +139,18 @@ export default function RoomList() {
 }
 
 // Component สำหรับแก้ไขข้อมูลผู้ใช้
-function EditRoom({
-  room,
-  updateRoom,
-  setEditingRoom,
+function EditBill({
+  bill,
+  updateBill,
+  setEditingBill,
 }: {
-  room: Room;
-  updateRoom: (user: Room) => void;
-  setEditingRoom: (user: Room | null) => void;
+  bill: Bill;
+  updateBill: (user: Bill) => void;
+  setEditingBill: (user: Bill | null) => void;
 }) {
-  const [name, setName] = useState(room.name);
-  const [status, setStatus] = useState(room.status);
-  const [userType, setUserType] = useState(room.userType);
+  const [name, setName] = useState(bill.name);
+  const [status, setStatus] = useState(bill.status);
+  const [userType, setUserType] = useState(bill.userType);
 
   return (
     <div className="p-4 bg-gray-100 rounded-lg">
@@ -188,7 +173,7 @@ function EditRoom({
       </select>
       <div className="flex space-x-2">
         <button
-          onClick={() => updateRoom({ ...room, name, id: 0 })}
+          onClick={() => updateBill({ ...bill, name, id: 0 })}
           className="btn-green"
         >
           บันทึก
